1145f52b3b13d3a2805fec6e4b7177051d6577d0
feiwang3311 <feiwang3311@gmail.com>
2018-02-16 09:10:10 -0500

    grad of dot

--- ad_lms_vector.scala
+++ ad_lms_vector.scala
@@ -10,7 +10,7 @@ object TEST1 {
 
 	trait VectorExp extends Dsl {
 
-		class Vector(val data: Rep[Array[Double]], val dim0: Rep[Int]) {
+		class Vector(val data: Rep[Array[Double]], val dim0: Int /*, val dim1: Int, val dim2: Int*/) {
 
 			def foreach(f: Rep[Double] => Rep[Unit]): Rep[Unit] =
 		    	for (i <- 0 until data.length) f(data(i))
@@ -21,28 +21,107 @@ object TEST1 {
 		    	foreach(x => if (f(x)) n += x); 
 		    	readVar(n) 
 		    }
+
 		    def dot(that: Vector) = {
 		    	// assert that and this have the same dimension
-		    	val res = var_new(0.0)
+		    	// fixME: the result should also be a vector of size 1, to keep the type consistent
+		    	val value = var_new(0.0)
 		    	for (i <- 0 until data.length) {
-		    		res += data(i) * that.data(i)
+		    		value += data(i) * that.data(i)
 		    	}
-		    	readVar(res)
+		    	// readVar(res)
+		    	val res = NewArray[Double](1)
+		    	res(0) = readVar(value)
+		    	new Vector(res, 1)
 		    }
+
+		    def + (that: Vector) = {
+		    	val res = NewArray[Double](dim0)
+		    	if (that.dim0 == 1) for (i <- (0 until dim0): Rep[Range]) res(i) = data(i) + that.data(0)
+		    	else if (dim0 == 1) for (i <- (0 until dim0): Rep[Range]) res(i) = data(0) + that.data(i)
+		    	else if (dim0 == that.dim0) for (i <- (0 until dim0): Rep[Range]) res(i) = data(i) + that.data(i)
+		    	else throw new IllegalArgumentException("dimensions of vector do not match!")
+		    	new Vector(res, dim0)
+		    }
+
+		    def - (that: Vector) = {
+		    	val res = NewArray[Double](dim0)
+		    	if (that.dim0 == 1) for (i <- (0 until dim0): Rep[Range]) res(i) = data(i) - that.data(0)
+		    	else if (dim0 == 1) for (i <- (0 until dim0): Rep[Range]) res(i) = data(0) - that.data(i)
+		    	else if (dim0 == that.dim0) for (i <- (0 until dim0): Rep[Range]) res(i) = data(i) - that.data(i)
+		    	else throw new IllegalArgumentException("dimensions of vector do not match!")
+		    	new Vector(res, dim0)
+		    }
+
+		    def * (that: Vector) = {
+		    	// element wise multiplication
+		    	val res = NewArray[Double](dim0)
+		    	if (that.dim0 == 1) for (i <- (0 until dim0): Rep[Range]) res(i) = data(i) * that.data(0)
+		    	else if (dim0 == 1) for (i <- (0 until dim0): Rep[Range]) res(i) = data(0) * that.data(i)
+		    	else if (dim0 == that.dim0) for (i <- (0 until dim0): Rep[Range]) res(i) = data(i) * that.data(i)
+		    	else throw new IllegalArgumentException("dimensions of vector do not match!")
+		    	new Vector(res, dim0)
+		    }
+
+		    def print() = {
+		    	for (i <- (0 until dim0): Rep[Range]) println(data(i))
+		    }
+
 		}
 
 		object Vector {
 
-			def randDouble() = unchecked[Double]("(double)rand()") // Fixme
+			def randDouble() = unchecked[Double]("(double)rand()") // Fixme, add srand(time)
 
 			def randinit(dim0: Int) = {
 				val res = NewArray[Double](dim0)
-				for (i <- (0 until dim0): Rep[Range]) {
-					res(i) = randDouble()
-				}
+				for (i <- (0 until dim0): Rep[Range]) res(i) = randDouble()
+				new Vector(res, dim0)
+			}
+
+			def zeros(dim0: Int) = {
+				val res = NewArray[Double](dim0)
+				for (i <- (0 until dim0): Rep[Range]) res(i) = 0.0
+				new Vector(res, dim0)
+			}
+
+			def ones(dim0: Int) = {
+				val res = NewArray[Double](dim0)
+				for (i <- (0 until dim0): Rep[Range]) res(i) = 1.0
 				new Vector(res, dim0)
 			}
 		}
+
+
+		// Tensor type is the same as NumR, just replace RDouble with Vector
+		type diff = cps[Unit]
+
+		class TensorR(val x: Vector, var d: Vector) {
+			def + (that: TensorR): TensorR @diff = shift { (k: TensorR => Unit) => 
+				val y = new TensorR(x + that.x, Vector.zeros(x.dim0)); k(y)
+				this.d += y.d; that.d += y.d
+			}
+			def - (that: TensorR): TensorR @diff = shift { (k: TensorR => Unit) =>
+				val y = new TensorR(x - that.x, Vector.zeros(x.dim0)); k(y)
+				this.d += y.d; that.d -= y.d
+			}
+			def * (that: TensorR): TensorR @diff = shift { (k: TensorR => Unit) =>
+				val y = new TensorR(x * that.x, Vector.zeros(x.dim0)); k(y)
+				this.d += that.x * y.d; that.d += this.x * y.d
+			}
+			def dot(that: TensorR): TensorR @diff = shift { (k: TensorR => Unit) =>
+				// assert both this and that are 1d vectors with the same size
+				val y = new TensorR(x dot that.x, Vector.zeros(1)); k(y)
+				this.d += that.x * y.d // broadcasting
+				that.d += this.x * y.d // broadcasting
+			}
+		}
+
+		def gradR(f: TensorR => TensorR @diff)(x: Vector): Vector = {
+	    	val x1 = new TensorR(x, Vector.zeros(x.dim0))
+	    	reset { f(x1).d = Vector.ones(1) } 
+	    	x1.d
+	    }
 
 	}
 
@@ -59,13 +138,35 @@ object TEST1 {
 
 				// val res3 = res map (t => 1.0) ERROR: map is not supported for C code generation
 				val result = res dot res2
-				printf("the result is %f", result)
+				result.print()
+				//val value = result.data(0)
+				//printf("the result is %f", value)
 			}
 
 		}
 
 		println(array1.code)
 		array1.eval("abc")
+
+		val array2 = new DslDriverC[String, Unit] with VectorExp {
+
+			def snippet(a: Rep[String]): Rep[Unit] = {
+				// read training data from file
+				val length = 2
+				val vector = NewArray[Double](length)
+				vector(0) = 2.0; vector(1) = 3.0			
+				// wrap as Vector type
+				val v = new Vector(vector, length)
+				// calculate gradient
+				val grad = gradR(t => t dot t)(v)
+				// show gradient
+				grad.print()
+			}
+
+		}
+
+		println(array2.code)
+		array2.eval("abc")
 
 	}
 }
