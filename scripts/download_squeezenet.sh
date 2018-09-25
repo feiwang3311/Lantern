7251a1f5a01a38629ba4a965b0d8bb3cce0aa9c2
Fei Wang <wang603@purdue.edu>
2018-09-25 17:08:26 -0400

    make download script work locally

--- scripts/download_squeezenet.sh
+++ scripts/download_squeezenet.sh
@@ -4,9 +4,10 @@ DIR=$HOME/onnx_models/squeezenet
 
 if [[ ! -d $DIR ]]
 then
-	rm -rf $HOME/tmp/*
+	mkdir -p $HOME/tmp
 	mkdir -p $HOME/onnx_models
 	wget https://s3.amazonaws.com/download.onnx/models/opset_9/squeezenet.tar.gz -O $HOME/tmp/squeezenet.tar.gz
 	tar xzf $HOME/tmp/squeezenet.tar.gz -C $HOME/onnx_models
+	rm -f $HOME/tmp/squeezenet.tar.gz
 fi
 
