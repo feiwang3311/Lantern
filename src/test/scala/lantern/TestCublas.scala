7af0c5d243007cfdb274b973547ccade324de95e
Dan Zheng <fangda.zheng@gmail.com>
2018-10-15 15:53:11 -0400

    Clean up cuBLAS tests.

--- src/test/scala/lantern/TestCublas.scala
+++ src/test/scala/lantern/TestCublas.scala
@@ -6,6 +6,8 @@ import org.scala_lang.virtualized.SourceContext
 class TestCublas extends LanternFunSuite {
   testGPU("vector-vector-dot") {
     val vvdot = new LanternDriverCublas[String, Unit] {
+      override val fileName = "lantern-gpu-vvdot"
+
       @virtualize
       def snippet(x: Rep[String]): Rep[Unit] = {
         val v1 = Tensor.fromData(Seq(4), 1, 2, 3, 4)
@@ -22,55 +24,39 @@ class TestCublas extends LanternFunSuite {
 
   testGPU("matrix-vector-dot") {
     val mvdot = new LanternDriverCublas[String, Unit] {
+      override val fileName = "lantern-gpu-mvdot"
+
       @virtualize
       def snippet(x: Rep[String]): Rep[Unit] = {
-        // NOTE: `cublasSgemv` behaves differently than CPU gemv implementation.
-        // This test fails for tensors with different scalar values.
-
-        // TODO: Fix this test for the following values:
         val m = Tensor.fromData(Seq(2, 4), 1, 2, 3, 4, 5, 6, 7, 8)
         val v = Tensor.fromData(Seq(4), -1, -2, -3, -4)
-
-        // val m = Tensor.ones(2, 4)
-        // val v = Tensor.ones(4)
         val result = m.dot(v).toCPU()
 
         backend = BackendCPU()
-        // val expected = Tensor.fill(Seq(2), 4)
         val expected = Tensor.fromData(Seq(2), -30, -70)
         result.print()
         Tensor.assertEqual(result, expected)
       }
     }
-    val file = "src/out/untested/vvd.cu"
-    val out = new java.io.PrintWriter(file)
-    out.println(mvdot.code)
-    out.close()
     runTest(mvdot)
   }
 
-  // TODO: Fix the other `dot` tests.
   testGPU("matrix-matrix-dot") {
-    val test = new LanternDriverCublas[String, Unit] {
+    val mmdot = new LanternDriverCublas[String, Unit] {
+      override val fileName = "lantern-gpu-mmdot"
+
       @virtualize
       def snippet(x: Rep[String]): Rep[Unit] = {
-        // NOTE: `cublasSgemm` behaves differently than CPU gemm implementation.
-        // This test fails for tensors with different scalar values.
-
-        // TODO: Fix this test for the following values:
         val m1 = Tensor.fromData(Seq(2, 3), 1, 2, 3, 4, 5, 6)
         val m2 = Tensor.fromData(Seq(3, 1), 2, 3, 4)
-
-        // val m1 = Tensor.ones(4, 4)
-        // val m2 = Tensor.ones(4, 4)
         val result = m1.dot(m2).toCPU()
 
         backend = BackendCPU()
-        // val expected = Tensor.fill(Seq(4, 4), 4)
         val expected = Tensor.fromData(Seq(2, 1), 20, 47)
+        result.print()
         Tensor.assertEqual(result, expected)
       }
     }
-    runTest(test)
+    runTest(mmdot)
   }
 }
