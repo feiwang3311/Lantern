d4629950100eb44a3ad9b6440041039219d56f62
feiwang3311 <feiwang3311@gmail.com>
2018-11-06 00:50:40 -0500

    pytorch treelstm GPU working

--- src/out/PLDI19evaluation/treelstm/pytorch/treeLSTM.py
+++ src/out/PLDI19evaluation/treelstm/pytorch/treeLSTM.py
@@ -25,8 +25,8 @@ with open(word_embedding_file, 'r') as f:
       word_embedding.append(np.asarray([float(i) for i in line.split()]).reshape(1, -1))
 
 word_embedding = np.concatenate(word_embedding, axis = 0)
-print(word_embedding.shape)
-print(word_embedding_length)
+word_embedding_GPU = Var(torch.Tensor(word_embedding)).cuda()
+
 
 # read tree_data
 tree_data_file = "array_tree.txt"
@@ -90,6 +90,7 @@ class TreeNet(nn.Module):
 
   # create a network for the xor problem given input and output
   def forward(self, scores, words, lchs, rchs):
+    scores_GPU = Var(torch.LongTensor(scores)).cuda()
     def rec(index):
       if (words[index] == -1):
         # branch node
@@ -103,23 +104,23 @@ class TreeNet(nn.Module):
         cell = i_gate * u_value + fl_gate * l_cell + fr_gate * r_cell
         hidden = o_gate * torch.tanh(cell)
         logits = (torch.matmul(self.Why, hidden) + self.by).view(1, output_size)
-        target = Var(torch.LongTensor([int(scores[index])]))
+        target = scores_GPU[index: index + 1]
         loss = l_loss + r_loss + F.nll_loss(F.log_softmax(logits, dim=1), target)
         return (loss, hidden, cell)
       else:
-        embedding_tensor = Var(torch.Tensor(word_embedding[words[index]]))
+        embedding_tensor = word_embedding_GPU[words[index]]
         i_gate = torch.sigmoid(torch.matmul(self.Wi, embedding_tensor) + self.bi)
         o_gate = torch.sigmoid(torch.matmul(self.Wo, embedding_tensor) + self.bo)
         u_value = torch.tanh(torch.matmul(self.Wu, embedding_tensor) + self.bu)
         cell = i_gate * u_value
         hidden = o_gate * torch.tanh(cell)
         logits = (torch.matmul(self.Why, hidden) + self.by).view(1, output_size)
-        target = Var(torch.LongTensor([int(scores[index])]))
+        target = scores_GPU[index: index + 1]
         loss = F.nll_loss(F.log_softmax(logits, dim=1), target)
         return (loss, hidden, cell)
     return rec(0)[0]
 
-net = TreeNet()
+net = TreeNet().cuda()
 opt = optim.Adagrad(net.parameters(), lr = learning_rate)
 
 epocNum = 6
